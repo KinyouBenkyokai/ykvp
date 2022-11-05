96c94eda235e70868854a3f8dae63ac202e632df
shiv3 <tshibaza@mercari.com>
2022-11-05 15:07:33 +0900

    Update

--- ybtestvc/main.go
+++ ybtestvc/main.go
@@ -6,10 +6,27 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("yubico-piv-tool", "-a", "verify-pin", "--sign", "-s", "9c", "-H", "SHA512", "-A", "RSA2048", "-i", "data.txt", "-o", "data.sig", "-P", "123456")
-	out, err := cmd.CombinedOutput()
-	fmt.Println(string(out))
+	// set data text
+	out, err := verify("aaa")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(out)
+}
+
+func verify(text string) ([]byte, error) {
+	cmd := exec.Command("yubico-piv-tool", "-a", "verify-pin", "--sign", "-s", "9c", "-H", "SHA512", "-A", "RSA2048", "-P", "123456")
+	fd0Pipe, _ := cmd.StdinPipe()
+
+	if _, err := fd0Pipe.Write([]byte(text)); err != nil {
+		return nil, err
+	}
+	if err := fd0Pipe.Close(); err != nil {
+		return nil, err
+	}
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
