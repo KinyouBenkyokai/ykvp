7f04f95f484aafd4052f38b08f3e0b1d4d7dcac0
kg0r0 <kg0r0@yahoo.co.jp>
2022-11-06 20:21:23 +0900

    Update

--- cmd/yuberifycli/main.go
+++ cmd/yuberifycli/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/kinyoubenkyokai/yuberify/lib/entity"
+	"github.com/kinyoubenkyokai/yuberify/lib/holder"
+	"github.com/kinyoubenkyokai/yuberify/lib/verifier"
 	"github.com/urfave/cli/v2"
 )
 
@@ -16,14 +21,10 @@ func main() {
 				Name:  "c",
 				Usage: "Verifiable Credential",
 			},
-			&cli.StringFlag{
+			&cli.IntFlag{
 				Name:  "p",
-				Value: "123456",
+				Value: 123456,
 				Usage: "PIN code",
-			},
-			&cli.StringFlag{
-				Name:  "s",
-				Usage: "Identifier for the only subject of the credential",
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
@@ -32,11 +33,26 @@ func main() {
 				log.Println("no verifiable credential specified")
 				return nil
 			}
-			pin := cCtx.String("p")
-			if pin == "" {
-				pin = "123456"
+			pin := cCtx.Int("p")
+			if pin == 0 {
+				pin = 123456
 			}
-			log.Println(vc)
+			credentials, err := entity.UnmarshalCredential([]byte(vc))
+			if err != nil {
+				panic(err)
+			}
+			nonce, err := verifier.MakeNonce()
+			if err != nil {
+				return err
+			}
+			presentation, err := holder.SignPresentation(
+				credentials,
+				nonce,
+				pin,
+			)
+
+			nicePrint(presentation, "Presentation")
+
 			log.Println(pin)
 			return nil
 		},
@@ -45,4 +61,12 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func nicePrint(i interface{}, name string) {
+	e := json.NewEncoder(os.Stdout)
+	e.SetIndent("", "  ")
+
+	fmt.Printf("\n***** %s *****\n\n", name)
+	e.Encode(i)
 }
